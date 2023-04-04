ce658d17e643e8eb8d51f5bbf04a3e7f8e043123
anggariasyahputra <anggariasyahputra2001@gmail.com>
2023-04-04 16:07:50 +0700

    first commit

---
+++ Kalkulator/Form1.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        LabelHasil = New Label()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button0 = New Button()
+        ButtonKoma = New Button()
+        Button13 = New Button()
+        ButtonTambah = New Button()
+        ButtonKurang = New Button()
+        ButtonKali = New Button()
+        ButtonHapus = New Button()
+        Button18 = New Button()
+        ButtonBagi = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(68, 285)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(118, 64)
+        Button1.TabIndex = 0
+        Button1.Text = "1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' LabelHasil
+        ' 
+        LabelHasil.AutoSize = True
+        LabelHasil.Font = New Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Point)
+        LabelHasil.Location = New Point(502, 5)
+        LabelHasil.Name = "LabelHasil"
+        LabelHasil.Size = New Size(56, 67)
+        LabelHasil.TabIndex = 1
+        LabelHasil.Text = "0"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(192, 285)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(118, 64)
+        Button2.TabIndex = 2
+        Button2.Text = "2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(316, 285)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(118, 64)
+        Button3.TabIndex = 3
+        Button3.Text = "3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(68, 215)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(118, 64)
+        Button4.TabIndex = 4
+        Button4.Text = "4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(192, 215)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(118, 64)
+        Button5.TabIndex = 5
+        Button5.Text = "5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(316, 215)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(118, 64)
+        Button6.TabIndex = 6
+        Button6.Text = "6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(68, 145)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(118, 64)
+        Button7.TabIndex = 7
+        Button7.Text = "7"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(192, 145)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(118, 64)
+        Button8.TabIndex = 8
+        Button8.Text = "8"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(316, 145)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(118, 64)
+        Button9.TabIndex = 9
+        Button9.Text = "9"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Button10.Location = New Point(68, 355)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(118, 64)
+        Button10.TabIndex = 10
+        Button10.Text = "+/-"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button0
+        ' 
+        Button0.Location = New Point(192, 355)
+        Button0.Name = "Button0"
+        Button0.Size = New Size(118, 64)
+        Button0.TabIndex = 11
+        Button0.Text = "0"
+        Button0.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonKoma
+        ' 
+        ButtonKoma.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonKoma.Location = New Point(316, 355)
+        ButtonKoma.Name = "ButtonKoma"
+        ButtonKoma.Size = New Size(118, 64)
+        ButtonKoma.TabIndex = 12
+        ButtonKoma.Text = ","
+        ButtonKoma.UseVisualStyleBackColor = True
+        ' 
+        ' Button13
+        ' 
+        Button13.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        Button13.Location = New Point(440, 355)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(118, 64)
+        Button13.TabIndex = 13
+        Button13.Text = "="
+        Button13.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonTambah
+        ' 
+        ButtonTambah.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonTambah.Location = New Point(440, 285)
+        ButtonTambah.Name = "ButtonTambah"
+        ButtonTambah.Size = New Size(118, 64)
+        ButtonTambah.TabIndex = 14
+        ButtonTambah.Text = "+"
+        ButtonTambah.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonKurang
+        ' 
+        ButtonKurang.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonKurang.Location = New Point(440, 215)
+        ButtonKurang.Name = "ButtonKurang"
+        ButtonKurang.Size = New Size(118, 64)
+        ButtonKurang.TabIndex = 15
+        ButtonKurang.Text = "-"
+        ButtonKurang.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonKali
+        ' 
+        ButtonKali.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonKali.Location = New Point(440, 145)
+        ButtonKali.Name = "ButtonKali"
+        ButtonKali.Size = New Size(118, 64)
+        ButtonKali.TabIndex = 16
+        ButtonKali.Text = "x"
+        ButtonKali.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonHapus
+        ' 
+        ButtonHapus.Location = New Point(68, 75)
+        ButtonHapus.Name = "ButtonHapus"
+        ButtonHapus.Size = New Size(118, 64)
+        ButtonHapus.TabIndex = 17
+        ButtonHapus.Text = "C"
+        ButtonHapus.UseVisualStyleBackColor = True
+        ' 
+        ' Button18
+        ' 
+        Button18.Location = New Point(192, 75)
+        Button18.Name = "Button18"
+        Button18.Size = New Size(242, 64)
+        Button18.TabIndex = 18
+        Button18.Text = "Back"
+        Button18.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonBagi
+        ' 
+        ButtonBagi.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonBagi.Location = New Point(440, 75)
+        ButtonBagi.Name = "ButtonBagi"
+        ButtonBagi.Size = New Size(118, 64)
+        ButtonBagi.TabIndex = 19
+        ButtonBagi.Text = "÷"
+        ButtonBagi.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(637, 459)
+        Controls.Add(ButtonBagi)
+        Controls.Add(Button18)
+        Controls.Add(ButtonHapus)
+        Controls.Add(ButtonKali)
+        Controls.Add(ButtonKurang)
+        Controls.Add(ButtonTambah)
+        Controls.Add(Button13)
+        Controls.Add(ButtonKoma)
+        Controls.Add(Button0)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(LabelHasil)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents LabelHasil As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents ButtonKoma As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents ButtonTambah As Button
+    Friend WithEvents ButtonKurang As Button
+    Friend WithEvents ButtonKali As Button
+    Friend WithEvents ButtonHapus As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents ButtonBagi As Button
+End Class
---
+++ Kalkulator/Form1.vb
@@ -0,0 +1,149 @@
+﻿Public Class Form1
+
+    Dim num1, num2 As Double
+    Dim op, timpa
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If LabelHasil.Text = "0" Or timpa = True Then
+            LabelHasil.Text = "1"
+            timpa = False
+        Else
+            LabelHasil.Text = LabelHasil.Text & "1"
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If LabelHasil.Text = "0" Or timpa = True Then
+            LabelHasil.Text = "2"
+            timpa = False
+        Else
+            LabelHasil.Text = LabelHasil.Text & "2"
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If LabelHasil.Text = "0" Or timpa = True Then
+            LabelHasil.Text = "3"
+            timpa = False
+        Else
+            LabelHasil.Text = LabelHasil.Text & "3"
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If LabelHasil.Text = "0" Or timpa = True Then
+            LabelHasil.Text = "4"
+            timpa = False
+        Else
+            LabelHasil.Text = LabelHasil.Text & "4"
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If LabelHasil.Text = "0" Or timpa = True Then
+            LabelHasil.Text = "5"
+            timpa = False
+        Else
+            LabelHasil.Text = LabelHasil.Text & "5"
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If LabelHasil.Text = "0" Or timpa = True Then
+            LabelHasil.Text = "6"
+            timpa = False
+        Else
+            LabelHasil.Text = LabelHasil.Text & "6"
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If LabelHasil.Text = "0" Or timpa = True Then
+            LabelHasil.Text = "7"
+            timpa = False
+        Else
+            LabelHasil.Text = LabelHasil.Text & "7"
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If LabelHasil.Text = "0" Or timpa = True Then
+            LabelHasil.Text = "8"
+            timpa = False
+        Else
+            LabelHasil.Text = LabelHasil.Text & "8"
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If LabelHasil.Text = "0" Or timpa = True Then
+            LabelHasil.Text = "9"
+            timpa = False
+        Else
+            LabelHasil.Text = LabelHasil.Text & "9"
+        End If
+    End Sub
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs)
+        If LabelHasil.Text = "0" Or timpa = True Then
+            LabelHasil.Text = "0"
+            timpa = False
+        Else
+            LabelHasil.Text = LabelHasil.Text & "0"
+        End If
+    End Sub
+
+    Private Sub ButtonKoma_Click(sender As Object, e As EventArgs)
+        If InStr(LabelHasil.Text, ",") = 0 Then
+            LabelHasil.Text = LabelHasil.Text & ","
+        End If
+    End Sub
+
+    Private Sub ButtonTambah_Click(sender As Object, e As EventArgs) Handles ButtonTambah.Click
+        timpa = True
+        op = "+"
+        num1 = LabelHasil.Text
+    End Sub
+
+    Private Sub ButtonKurang_Click(sender As Object, e As EventArgs) Handles ButtonKurang.Click
+        timpa = True
+        op = "-"
+        num1 = LabelHasil.Text
+    End Sub
+
+    Private Sub ButtonKali_Click(sender As Object, e As EventArgs) Handles ButtonKali.Click
+        timpa = True
+        op = "*"
+        num1 = LabelHasil.Text
+    End Sub
+
+    Private Sub ButtonBagi_Click(sender As Object, e As EventArgs) Handles ButtonBagi.Click
+        timpa = True
+        op = "/"
+        num1 = LabelHasil.Text
+    End Sub
+
+    Private Sub ButtonHasil_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Select Case op
+            Case "+"
+                LabelHasil.Text = Val(num1) + Val(LabelHasil.Text)
+                timpa = True
+            Case "-"
+                LabelHasil.Text = Val(num1) - Val(LabelHasil.Text)
+                timpa = True
+            Case "/"
+                LabelHasil.Text = Val(num1) / Val(LabelHasil.Text)
+                timpa = True
+            Case "*"
+                LabelHasil.Text = Val(num1) * Val(LabelHasil.Text)
+                timpa = True
+        End Select
+
+    End Sub
+
+    Private Sub ButtonHapus_Click(sender As Object, e As EventArgs) Handles ButtonHapus.Click
+        LabelHasil.Text = "0"
+        num1 = 0
+        op = ""
+    End Sub
+End Class
